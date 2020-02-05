808cb9fa41dd4fa94b96265dd8ca2a82c970ab1c
nemanja13 <nemanja.maksimovic.4.18@ict.edu.rs>
2020-02-05 21:09:38 +0100

    index sadrzaj

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ $(document).ready(function(){
     podMeni();
     function podMeni(){
         $("#telefonija").hover(function(){
-            $("#podMeniTelefonija").stop( true, true ).show();
+            $("#podMeniTelefonija").show();
         },
         function(){
             $("#podMeniTelefonija").hide();
         });
         $("#korisnickaPodrska").hover(function(){
-            $("#podMeniKorisnickaPodrska").stop( true, true ).show();
+            $("#podMeniKorisnickaPodrska").show();
         },function(){
-            $("#podMeniKorisnickaPodrska").stop( true, true ).hide();
+            $("#podMeniKorisnickaPodrska").hide();
         });
         $(document).click(function(){
-            $("#podMeniKorisnickaPodrska").stop( true, true ).hide();
-            $("#podMeniTelefonija").stop( true, true ).hide();
+            $("#podMeniKorisnickaPodrska").hide();
+            $("#podMeniTelefonija").hide();
         });
         $("#podMeniTelefonija").hover(function(){ $(this).show()},function(){ $(this).hide()});
         $("#podMeniKorisnickaPodrska").hover(function(){ $(this).show()},function(){ $(this).hide()});
@@ -29,20 +29,20 @@ $(document).ready(function(){
     }
     
     slajder();
-    function slajder(){
-        var interval=setInterval(function(){
-            var aktivan=$("#slajder .vidljiv");
-            var sledeci=aktivan.next().length?$(".vidljiv").next():aktivan.parent().children(":first");
-            aktivan.removeClass("vidljiv").addClass("nevidljiv");
-            sledeci.addClass("vidljiv").removeClass("nevidljiv");
-    }, 10000);
-    var strelicaDesno=$(".strelicaDesno");
-    var strelicaLevo=$(".strelicaLevo");
-    strelicaDesno.click(function(){
+    function slideShow(){
         var aktivan=$("#slajder .vidljiv");
         var sledeci=aktivan.next().length?$(".vidljiv").next():aktivan.parent().children(":first");
         aktivan.removeClass("vidljiv").addClass("nevidljiv");
         sledeci.addClass("vidljiv").removeClass("nevidljiv");
+    }
+    function slajder(){
+        var interval=setInterval(function(){
+            slideShow();
+    }, 10000);
+    var strelicaDesno=$(".strelicaDesno");
+    var strelicaLevo=$(".strelicaLevo");
+    strelicaDesno.click(function(){
+        slideShow();
     });
     strelicaLevo.click(function(){
         var aktivan=$("#slajder .vidljiv");
