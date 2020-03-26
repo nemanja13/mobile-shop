6020717ff0ad28e76893e6739a6665d5f66139ae
nemanja13 <nemanja.maksimovic.4.18@ict.edu.rs>
2020-03-26 01:21:06 +0100

    gotov sajt

--- js/main.js
+++ js/main.js
@@ -1,4 +1,157 @@
 var url=location.href
+if(url.indexOf("index.html")!=-1){
+    window.onload=function(){
+        prikazMenija();
+        prikazSlajdera();
+        oNama();
+        $.ajax({
+            url: "data/proizvodi.json",
+            type:"GET",
+            dataType:"json",
+            success:function(data){
+                ispisNajpopularnijih(data);
+                prikaziSaznajViseIBlokovi();
+                $("#najnovije").click(function(e){
+                    e.preventDefault();
+                    $("#najpopularnije").removeClass("aktivan");
+                    $("#najnovije").addClass("aktivan");
+                    ispisNajnovijih(data);
+                    prikaziSaznajViseIBlokovi();
+                });
+                $("#najpopularnije").click(function(e){
+                    e.preventDefault();
+                    $("#najnovije").removeClass("aktivan");
+                    $("#najpopularnije").addClass("aktivan");
+                    ispisNajpopularnijih(data);
+                    prikaziSaznajViseIBlokovi();
+                });
+            },
+            error:function(err){
+                console.error(err);
+            }
+        });
+        
+        (function(){
+            setInterval(function(){
+                slideShow();
+        }, 10000);
+        var strelicaDesno=$(".strelicaDesno");
+        var strelicaLevo=$(".strelicaLevo");
+        strelicaDesno.click(function(){
+            slideShow();
+        });
+        strelicaLevo.click(function(){
+            var aktivan=$("#slajder .vidljiv");
+            var sledeci=aktivan.prev().length?$(".vidljiv").prev():aktivan.parent().children(":last");
+            aktivan.removeClass("vidljiv").addClass("nevidljiv");
+            sledeci.addClass("vidljiv").removeClass("nevidljiv");
+        })
+        })();
+        function slideShow(){
+            var aktivan=$("#slajder .vidljiv");
+            var sledeci=aktivan.next().length?$(".vidljiv").next():aktivan.parent().children(":first");
+            aktivan.removeClass("vidljiv").addClass("nevidljiv");
+            sledeci.addClass("vidljiv").removeClass("nevidljiv");
+        }
+        function prikaziSaznajViseIBlokovi(){
+            var velikiBlok=$(".velikiBlok");
+            var maliBlok=$(".maliBlok");
+            prikaziSaznajVise(velikiBlok);
+            prikaziSaznajVise(maliBlok);
+        } 
+        function prikaziSaznajVise(blok){
+            $(blok).hover(function(){
+                $(this).find("a").stop(true, true).fadeIn();
+                $(this).find("img").stop(true, true).animate({"opacity": 0.5}, "slow");
+                $(this).find("h3").stop(true, true).animate({"opacity": 0.5}, "slow");
+                $(this).stop(true, true).css({"boxShadow" : "5px 5px 10px 5px #c2c2c2", "transition" : "box-shadow 1s"})
+            },function(){
+                $(this).find("a").stop(true, true).fadeOut();
+                $(this).find("img").stop(true, true).animate({"opacity": 1}, "slow");
+                $(this).find("h3").stop(true, true).animate({"opacity": 1}, "slow");
+                $(this).stop(true, true).css({"boxShadow" : "none"})
+            });
+        }
+        function focus(input){
+            $(input).focus(function(){
+                $(this).prev().css("color",  "rgb(115, 164, 255)");
+            });
+            $(input).blur(function(){
+                $(this).prev().css("color",  "rgb(0, 0, 0)");
+            });
+        }
+        focus($("input"));
+        focus($("textarea"));
+    
+    
+    }
+    function ispisNajpopularnijih(data){
+        data.sort(function(a,b){
+            if(a.najpopularniji==b.najpopularniji) return 0;
+            return a.najpopularniji>b.najpopularniji? -1: 1;
+        });
+        
+        ispisNajnovijihNajpopularnijih(data.slice(0,5));
+    }
+    function ispisNajnovijih(data){
+        data.sort(function(a,b){
+            let datum1=new Date(a.najnoviji);
+            let datum2=new Date(b.najnoviji);
+            return (Date.UTC(datum2.getFullYear(), datum2.getMonth(), datum2.getDate())-Date.UTC(datum1.getFullYear(), datum1.getMonth(), datum1.getDate()));
+        });
+        ispisNajnovijihNajpopularnijih(data.slice(0,5));
+    }
+    function ispisNajnovijihNajpopularnijih(noviNiz){
+        let ispis=`
+            <div class="col-lg-5 col-11 flex2 velikiBlok">
+                <img src="${noviNiz[0].slika.putanja}" alt="${noviNiz[0].marka} ${noviNiz[0].model}"/>
+                <h3>${noviNiz[0].marka} ${noviNiz[0].model}</h3>
+                <a href="#" class="nevidljiv dodajUKorpu" data-id="${noviNiz[0].id}"><i class="fas fa-shopping-cart"></i> Dodaj u korpu</a>
+            </div>
+            <div class="col-lg-6 col-11 flex3 prviRed">`;
+
+        for(let i=1; i<noviNiz.length; i++){
+            ispis+=`
+            <div class="col-12 col-sm-5 maliBlok flex2">
+                <img src="${noviNiz[i].slika.putanja}" alt="${noviNiz[i].marka} ${noviNiz[i].model}"/>
+                <h3>${noviNiz[i].marka} ${noviNiz[i].model}</h3>
+                <a href="#" class="nevidljiv dodajUKorpu" data-id="${noviNiz[i].id}"><i class="fas fa-shopping-cart"></i> Dodaj u korpu</a>
+            </div>`;
+        }
+                
+        ispis+=`</div>`;
+        document.getElementById("sadrzaj").innerHTML=ispis;
+        $(".dodajUKorpu").click(dodajUKorpu);
+    }
+}
+if(url.indexOf("telefoni.html")!=-1){
+    prikazMenija();
+    tip("telefon");
+    prikaziFilter();
+}
+if(url.indexOf("tableti.html")!=-1){
+    prikazMenija();
+    tip("tablet");
+    prikaziFilter();
+}
+if(url.indexOf("dozTelefon.html")!=-1){
+    prikazMenija();
+    tip("dozTelefon");
+    prikaziFilter();
+}
+if(url.indexOf("dozTablet.html")!=-1){
+    prikazMenija();
+    tip("dozTablet");
+    prikaziFilter();
+}
+if(url.indexOf("korpa.html")!=-1){
+    prikazMenija();
+    korpa();
+}
+
+if(url.indexOf("autor.html")!=-1){
+    prikazMenija();
+}
 function podMeni(){
     $("#telefonija").hover(function(){
         $("#podMeniTelefonija").show();
@@ -146,163 +299,6 @@ function brojProizvodaUKorpi(){
         $("#header .broj").hide();
     }
 }
-if(url.indexOf("index.html")!=-1){
-    window.onload=function(){
-        prikazMenija();
-        prikazSlajdera();
-        oNama();
-        $.ajax({
-            url: "data/proizvodi.json",
-            type:"GET",
-            dataType:"json",
-            success:function(data){
-                ispisNajpopularnijih(data);
-                prikaziSaznajViseIBlokovi();
-                $("#najnovije").click(function(e){
-                    e.preventDefault();
-                    $("#najpopularnije").removeClass("aktivan");
-                    $("#najnovije").addClass("aktivan");
-                    ispisNajnovijih(data);
-                    prikaziSaznajViseIBlokovi();
-                });
-                $("#najpopularnije").click(function(e){
-                    e.preventDefault();
-                    $("#najnovije").removeClass("aktivan");
-                    $("#najpopularnije").addClass("aktivan");
-                    ispisNajpopularnijih(data);
-                    prikaziSaznajViseIBlokovi();
-                });
-            },
-            error:function(err){
-                console.error(err);
-            }
-        });
-    
-    
-        slajder();
-        function slideShow(){
-            var aktivan=$("#slajder .vidljiv");
-            var sledeci=aktivan.next().length?$(".vidljiv").next():aktivan.parent().children(":first");
-            aktivan.removeClass("vidljiv").addClass("nevidljiv");
-            sledeci.addClass("vidljiv").removeClass("nevidljiv");
-        }
-        function slajder(){
-            setInterval(function(){
-                slideShow();
-        }, 10000);
-        var strelicaDesno=$(".strelicaDesno");
-        var strelicaLevo=$(".strelicaLevo");
-        strelicaDesno.click(function(){
-            slideShow();
-        });
-        strelicaLevo.click(function(){
-            var aktivan=$("#slajder .vidljiv");
-            var sledeci=aktivan.prev().length?$(".vidljiv").prev():aktivan.parent().children(":last");
-            aktivan.removeClass("vidljiv").addClass("nevidljiv");
-            sledeci.addClass("vidljiv").removeClass("nevidljiv");
-        })
-        }
-    
-        function prikaziSaznajViseIBlokovi(){
-            var velikiBlok=$(".velikiBlok");
-            var maliBlok=$(".maliBlok");
-            prikaziSaznajVise(velikiBlok);
-            prikaziSaznajVise(maliBlok);
-        }
-        
-        function prikaziSaznajVise(blok){
-            $(blok).hover(function(){
-                $(this).find("a").stop(true, true).fadeIn();
-                $(this).find("img").stop(true, true).animate({"opacity": 0.5}, "slow");
-                $(this).find("h3").stop(true, true).animate({"opacity": 0.5}, "slow");
-                $(this).stop(true, true).css({"boxShadow" : "5px 5px 10px 5px #c2c2c2", "transition" : "box-shadow 1s"})
-            },function(){
-                $(this).find("a").stop(true, true).fadeOut();
-                $(this).find("img").stop(true, true).animate({"opacity": 1}, "slow");
-                $(this).find("h3").stop(true, true).animate({"opacity": 1}, "slow");
-                $(this).stop(true, true).css({"boxShadow" : "none"})
-            });
-        }
-        function focus(input){
-            $(input).focus(function(){
-                $(this).prev().css("color",  "rgb(115, 164, 255)");
-            });
-            $(input).blur(function(){
-                $(this).prev().css("color",  "rgb(0, 0, 0)");
-            });
-        }
-        focus($("input"));
-        focus($("textarea"));
-    
-    
-    }
-    function ispisNajpopularnijih(data){
-        data.sort(function(a,b){
-            if(a.najpopularniji==b.najpopularniji) return 0;
-            return a.najpopularniji>b.najpopularniji? -1: 1;
-        });
-        
-        ispisNajnovijihNajpopularnijih(data.slice(0,5));
-    }
-    function ispisNajnovijih(data){
-        data.sort(function(a,b){
-            let datum1=new Date(a.najnoviji);
-            let datum2=new Date(b.najnoviji);
-            return (Date.UTC(datum2.getFullYear(), datum2.getMonth(), datum2.getDate())-Date.UTC(datum1.getFullYear(), datum1.getMonth(), datum1.getDate()));
-        });
-        ispisNajnovijihNajpopularnijih(data.slice(0,5));
-    }
-    function ispisNajnovijihNajpopularnijih(noviNiz){
-        let ispis=`
-            <div class="col-lg-5 col-11 flex2 velikiBlok">
-                <img src="${noviNiz[0].slika.putanja}" alt="${noviNiz[0].marka} ${noviNiz[0].model}"/>
-                <h3>${noviNiz[0].marka} ${noviNiz[0].model}</h3>
-                <a href="#" class="nevidljiv dodajUKorpu" data-id="${noviNiz[0].id}"><i class="fas fa-shopping-cart"></i> Dodaj u korpu</a>
-            </div>
-            <div class="col-lg-6 col-11 flex3 prviRed">`;
-
-        for(let i=1; i<noviNiz.length; i++){
-            ispis+=`
-            <div class="col-12 col-sm-5 maliBlok flex2">
-                <img src="${noviNiz[i].slika.putanja}" alt="${noviNiz[i].marka} ${noviNiz[i].model}"/>
-                <h3>${noviNiz[i].marka} ${noviNiz[i].model}</h3>
-                <a href="#" class="nevidljiv dodajUKorpu" data-id="${noviNiz[i].id}"><i class="fas fa-shopping-cart"></i> Dodaj u korpu</a>
-            </div>`;
-        }
-                
-        ispis+=`</div>`;
-        document.getElementById("sadrzaj").innerHTML=ispis;
-        $(".dodajUKorpu").click(dodajUKorpu);
-    }
-}
-if(url.indexOf("telefoni.html")!=-1){
-    prikazMenija();
-    tip("telefon");
-    prikaziFilter();
-}
-if(url.indexOf("tableti.html")!=-1){
-    prikazMenija();
-    tip("tablet");
-    prikaziFilter();
-}
-if(url.indexOf("dozTelefon.html")!=-1){
-    prikazMenija();
-    tip("dozTelefon");
-    prikaziFilter();
-}
-if(url.indexOf("dozTablet.html")!=-1){
-    prikazMenija();
-    tip("dozTablet");
-    prikaziFilter();
-}
-if(url.indexOf("korpa.html")!=-1){
-    prikazMenija();
-    korpa();
-}
-
-if(url.indexOf("autor.html")!=-1){
-    prikazMenija();
-}
 function upisiULocalStorage(string, vrednost){
     if(localStorage){
         localStorage.setItem(string, JSON.stringify(vrednost));
@@ -355,11 +351,9 @@ function prikazCheckbox(niz, name, div){
 function sortiraj() {
     let val= this.value;
            let proizvodi=sviProizvodi();
-           if(localStorage.getItem("filtriraniProizvodi")){
+           if(localStorage.getItem("filtriraniProizvodi") && (nizMarki.length!=0 || nizRamMem.length!=0 || nizIntMem.length!=0 || nizRezolucija.length!=0)){
                proizvodi=JSON.parse(localStorage.getItem("filtriraniProizvodi"));
-               console.log(proizvodi)
            }
-           console.log(proizvodi)
            proizvodi.sort(function(a,b) {
             if(val=="cenaDesc"){
                 let prvi=parseFloat(a.cena.nova);
@@ -386,18 +380,18 @@ function sortiraj() {
                 return;
             }
         });
-        upisiULocalStorage("proizvodi", proizvodi);
-        upisiULocalStorage("filtriraniProizvodi", proizvodi);
         ispisProizvoda(proizvodi);
 }
 var nizMarki=[];
 var nizRamMem=[];
 var nizRezolucija=[];
 var nizIntMem=[];
+function svaFiltriranja(){
 function filtriranjePoMarci(){
     
     let val=this.value;
     document.getElementById("search").value="";
+    document.getElementById("sortiraj").value=0;
     if(!nizMarki.includes(val)){
         nizMarki.push(val);
     }else{
@@ -432,6 +426,7 @@ function filtriranjePoRamMem(){
     
     let val=this.value;
     document.getElementById("search").value="";
+    document.getElementById("sortiraj").value=0;
     if(!nizRamMem.includes(val)){
         nizRamMem.push(val);
     }else{
@@ -477,6 +472,7 @@ function filtriranjePoIntMem(){
     
     let val=this.value;
     document.getElementById("search").value="";
+    document.getElementById("sortiraj").value=0;
     if(!nizIntMem.includes(val)){
         nizIntMem.push(val);
     }else{
@@ -522,6 +518,7 @@ function filtriranjePoRezoluciji(){
     
     let val=this.value;
     document.getElementById("search").value="";
+    document.getElementById("sortiraj").value=0;
     if(!nizRezolucija.includes(val)){
         nizRezolucija.push(val);
     }else{
@@ -586,7 +583,16 @@ function filtriranjeSearch(){
         nemaProizvoda();
     }
 }
+    return {
+        filtriranjePoMarci,
+        filtriranjePoIntMem,
+        filtriranjePoRamMem,
+        filtriranjePoRezoluciji,
+        filtriranjeSearch
+    }
+}
 function tip(tip){
+    var filtriranje=svaFiltriranja();
     localStorage.removeItem("filtriraniProizvodiPoRamMem");
     localStorage.removeItem("filtriraniProizvodiPoIntMem");
     localStorage.removeItem("filtriraniProizvodiPoRez");
@@ -605,17 +611,18 @@ function tip(tip){
                     inputCheckbox();
                 else
                     inputCheckbox2();
-                $("input[name='robnaMarka']").click(filtriranjePoMarci);
-                $("input[name='ramMemorija']").click(filtriranjePoRamMem);
-                $("input[name='internaMemorija']").click(filtriranjePoIntMem);
-                $("input[name='rezolucija']").click(filtriranjePoRezoluciji);
+                
+                $("input[name='robnaMarka']").click(filtriranje.filtriranjePoMarci);
+                $("input[name='ramMemorija']").click(filtriranje.filtriranjePoRamMem);
+                $("input[name='internaMemorija']").click(filtriranje.filtriranjePoIntMem);
+                $("input[name='rezolucija']").click(filtriranje.filtriranjePoRezoluciji);
             },
             error:function(err){
                 console.error(err);
             }
         });
         document.getElementById("sortiraj").addEventListener("change", sortiraj);
-        document.getElementById("search").addEventListener("keyup", filtriranjeSearch);
+        document.getElementById("search").addEventListener("keyup", filtriranje.filtriranjeSearch);
     }
 }
 function ispisProizvoda(proizvodi){
